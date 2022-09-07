84827090d855f1822cb38b1c1e41c1d2105d4bee
CyrilG <gardenat.cyril@cairn-devices.eu>
2022-09-07 13:43:18 +0200

    new path system

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use qmetaobject::QJsonObject;
 use qmetaobject::{qt_base_class, qt_method, qt_property, qt_signal, QJsonArray, QObject, QString};
 use serde_json::Value;
-use std::borrow::Borrow;
 use std::collections::HashMap;
+use std::env;
 use std::fs::read_to_string;
 use sys_locale::get_locale;
 
@@ -21,8 +21,8 @@ pub struct QTranslater {
     lang: String,      // current language --> Default : your computer language
     qlang: qt_property!(QString; NOTIFY qlang_changed),
 
-    init: qt_method!(fn(&mut self)),         // Constructor method
-    refresh_dict: qt_method!(fn(&mut self)), //to refresh datas
+    init: qt_method!(fn(&mut self, folder_path: String)), // Constructor method
+    refresh_dict: qt_method!(fn(&mut self)),              //to refresh datas
     change_lang: qt_method!(fn(&mut self, new_lang: QString)), // Methode to switch to change language
 
     dict_changed: qt_signal!(), //just a qt_signal() that is triggered when modifications are done so the changes are displayed
@@ -30,9 +30,31 @@ pub struct QTranslater {
 }
 
 impl QTranslater {
-    pub fn init(&mut self) {
+    pub fn init(&mut self, folder_path: String) {
         self.lang = get_locale().unwrap_or_else(|| String::from("en-US")); //Get your computer lang
-        self.json_path = "./src/lang/".to_string() + self.lang.borrow() + ".json";
+
+        if folder_path == "dev_path" {
+            self.json_path = "./src/lang/".to_string() + &self.lang + ".json";
+        } else if folder_path == "installed_path" {
+            match env::var("SUDO_USER") {
+                Ok(v) => {
+                    self.json_path = "/home/".to_string()
+                        + &v
+                        + "/.local/share/cairn-grace/lang/"
+                        + &self.lang
+                        + ".json"
+                }
+                Err(_) => {
+                    self.json_path = (env::var("HOME").expect("cannot reach home"))
+                        + "/.local/share/cairn-grace/lang/"
+                        + &self.lang
+                        + ".json"
+                }
+            }
+        } else {
+            self.json_path = folder_path + &self.lang + ".json";
+        };
+
         let mut qlang = self.lang.clone();
 
         let data: String;
@@ -40,11 +62,12 @@ impl QTranslater {
             Ok(v) => data = v,
             Err(_err) => {
                 data = read_to_string("./src/lang/en_GB.json").expect("can't find english dict");
+                self.lang = "en_GB".to_string();
                 qlang = "en_GB".to_string()
             }
         }
 
-        //let data = read_to_string(&self.json_path).expect("file not found"); //Charge json data from a string
+        //Charge json data from a string
 
         let mut lookup: HashMap<String, Value> = serde_json::from_str(&data).unwrap();
         let mut keys: Vec<String> = Vec::new(); //All the keys will be stored in this Vec
@@ -71,7 +94,6 @@ impl QTranslater {
     fn change_lang(&mut self, new_lang: QString) {
         let qlang = new_lang.clone();
         self.lang = new_lang.to_string();
-        self.json_path = "./src/lang/".to_string() + self.lang.borrow() + ".json";
         let data = read_to_string(&self.json_path).expect("file not found");
 
         let mut lookup: HashMap<String, Value> = serde_json::from_str(&data).unwrap();
